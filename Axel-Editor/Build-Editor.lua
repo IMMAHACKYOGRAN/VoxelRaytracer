91803118377ab785ce43960eed1c0c5f1565ec22
IMMAHACKYOGRAN <71002312+IMMAHACKYOGRAN@users.noreply.github.com>
2024-05-14 12:40:05 +1000

    Update Build-Editor.lua

--- Axel-Editor/Build-Editor.lua
+++ Axel-Editor/Build-Editor.lua
@@ -19,6 +19,7 @@ project "Axel-Editor"
         "../Axel-Core/vendor/spdlog/include",
         "../Axel-Core/vendor/GLFW/include",
         "../Axel-Core/vendor/imgui",
+        "../Axel-Core/vender/glm",
     }
 
     links
